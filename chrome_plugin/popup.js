647e315cec93a2293dae261c6f94b5c49e37ffa0
Sai Teja Pratap <pratap130492@gmail.com>
2016-10-22 13:40:59 -0700

    Chrome plugin for bookmarks

---
+++ chrome_plugin/popup.js
@@ -0,0 +1,44 @@
+function setShortAlias(tabs) {
+    var short = $("#short_id").val();
+    var secret = $("#secret").val();
+    console.log(short);
+    console.log(secret);
+    console.log(tabs);
+    var url = tabs[0].url;
+    console.log(url);
+    $.ajax({
+        url: 'https://yesteapea.com/bm/actions/add',
+        type: 'POST',
+        dataType: "html",
+        data: {
+            short: short,
+            url: url,
+            secret: secret
+        },
+        success: function(response) {
+            console.log(response);
+            if (response == "ok") {
+                var s_url = `http://suram.in/r/${short}`;
+                var message = `Row added at <a href='${s_url}'>${s_url}</a>`;
+                $("#result").html(message);
+            } else {
+                $("#result").html(response);
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, exception) {
+            $("#result").html(textStatus);
+        },
+        async: true
+    });
+}
+
+$(function() {
+    $('#button').click(
+        function() {
+            var query = {
+                active: true,
+                currentWindow: true
+            };
+            chrome.tabs.query(query, setShortAlias);
+        });
+});
